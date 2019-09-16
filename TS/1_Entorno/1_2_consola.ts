27dd990f5b8331c83c619ea1b55d35188f1ccc3f
Marcos Huck <marcos.h3012@gmail.com>
2019-09-16 08:44:52 -0300

    Elimina redundancia

--- TS/1_Entorno/1_2_consola.ts
+++
@@ -1,6 +0,0 @@
-console.log('Log');
-console.debug('Prueba');
-console.info('Info');
-console.warn('Advertir');
-console.error('Error');
-console.clear();
--- Typescript/1_Entorno/1_1_consola.ts
+++
@@ -1,5 +0,0 @@
-console.log('Log');
-console.debug('Prueba');
-console.info('Info');
-console.warn('Advertir');
-console.error('Error');
