ed746f074f7bd49ed217d3b87b9d4f5a9b85af73
Marcos Huck <marcos.h3012@gmail.com>
2019-09-16 11:42:51 -0300

    Agrega parametros de transpilacion

--- TypeScript/3_Arrays/3_8_indexOf.ts
+++ TypeScript/3_Arrays/3_8_indexOf.ts
@@ -1,7 +1,7 @@
-function main() {
+function main(): void {
   const edades: number[] = [55, 33, 22, 11, 99, 66];
 
-  const index: number = edades.indexOf(56);
+  const index: number = edades.indexOf(99);
 
   if(index === -1) {
     return;
