f459150cd730f84fbca23072fa6a5596820d2c8c
Marcos Huck <marcos.h3012@gmail.com>
2019-09-16 11:49:01 -0300

    Agrega ejemplo de concat

---
+++ TypeScript/3_Arrays/3_9_concat.ts
@@ -0,0 +1,6 @@
+(() => {
+  const grupoEdadesUno: number[] = [55, 33, 22, 11, 99, 66];
+  const grupoEdadesDos: number[] = [56, 43, 12, 25, 64, 92];
+  const edades: number[] = grupoEdadesUno.concat(grupoEdadesDos);
+  console.log(edades);
+})();
